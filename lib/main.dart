92fcbf013705304dabf064f0070a1fe49162d419
EmanT <tharwateman38@gmail.com>
2025-05-16 11:42:35 +0300

    bookly app

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:bookly/run_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Bookly());
+}
---
+++ lib/run_app.dart
@@ -0,0 +1,16 @@
+import 'package:bookly/constants.dart';
+import 'package:bookly/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: ThemeData(scaffoldBackgroundColor: kPrimaryColor),
+      home: const SplashView(),
+    );
+  }
+}
